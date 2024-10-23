27d2b48b12b5d9d592d29878fc85a21a12d24258
Nerdeee <george.larres@gmail.com>
2024-10-23 14:00:05 -0700

    Added image processing and features and a function to properly organize images

--- datapreprocessing.py
+++ datapreprocessing.py
@@ -6,10 +6,22 @@ import os
 import matplotlib.pyplot as plt
 import random
 import pickle
+import re
 
 NEW_SIZE = 300
 
-img_folder = os.path.join("/",  "images")
-for img in img_folder:
-    img_array = cv2.imread(img)
-    img_array = cv2.resize(img_array, (NEW_SIZE, NEW_SIZE))
+abs_path = os.path.dirname(os.path.abspath(__file__))
+
+def createDirectories():
+    male_pattern = re.compile(r'^...M')
+    img_folder = os.path.join(abs_path, "SOCOFing")
+    for img in os.listdir(img_folder):
+        if male_pattern.match(img):
+            # TO DO: Create respective directories, look more into fingerprint recognition techniques/algorithms/etc
+        
+
+def processImages():
+    img_folder = os.path.join(abs_path,  "images")
+    for img in img_folder:
+        img_array = cv2.imread(img)
+        img_array = cv2.resize(img_array, (NEW_SIZE, NEW_SIZE))
