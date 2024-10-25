9f31d56ceb1c0a3f2bf96333a8c64571b7c426ed
Nerdeee <george.larres@gmail.com>
2024-10-25 13:40:42 -0700

    Added more code for organizing images

--- datapreprocessing.py
+++ datapreprocessing.py
@@ -7,21 +7,40 @@ import matplotlib.pyplot as plt
 import random
 import pickle
 import re
+import shutil
 
 NEW_SIZE = 300
 
 abs_path = os.path.dirname(os.path.abspath(__file__))
+re_exceptions = 0
+male_dirname = "Male Fingers"
+female_dirname = "Female Fingers"
 
 def createDirectories():
-    male_pattern = re.compile(r'^...M')
+    male_pattern = re.compile(r'.*M.*')
+    female_pattern = re.compile(r'.*F.*')
     img_folder = os.path.join(abs_path, "SOCOFing")
     for img in os.listdir(img_folder):
         if male_pattern.match(img):
-            # TO DO: Create respective directories, look more into fingerprint recognition techniques/algorithms/etc
-        
+            # TO DO: Split the images in the gender directories into directories for each finger
+            if os.path.isdir(male_dirname):
+                shutil.copy(img, male_dirname)   
+            else:
+                os.mkdir(male_dirname)
+        elif female_pattern.match(img):
+            if os.path.isdir(female_dirname):
+                shutil.copy(img, female_dirname)   
+            else:
+                os.mkdir(female_dirname)
+        else:
+            re_exceptions += 1
 
 def processImages():
+    # TO DO: look more into fingerprint recognition techniques/algorithms/etc, the code below will also need to be tweaked
     img_folder = os.path.join(abs_path,  "images")
     for img in img_folder:
         img_array = cv2.imread(img)
         img_array = cv2.resize(img_array, (NEW_SIZE, NEW_SIZE))
+
+
+print("Number of images that couldn't be categorized: ", re_exceptions)
