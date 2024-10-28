b5e41541fbc83f5e433165a3ef404aa94886cfa2
Nerdeee <george.larres@gmail.com>
2024-10-28 00:47:52 -0700

    Finished folder structure and categorizing images function. Next, will look into fingerprint recognition techniques and algorithms

--- datapreprocessing.py
+++ datapreprocessing.py
@@ -19,21 +19,70 @@ female_dirname = "Female Fingers"
 def createDirectories():
     male_pattern = re.compile(r'.*M.*')
     female_pattern = re.compile(r'.*F.*')
+    left_patterns = {
+        'thumb': re.compile(r'.*Left_thumb.*'),
+        'index': re.compile(r'.*Left_index.*'),
+        'middle': re.compile(r'.*Left_middle.*'),
+        'ring': re.compile(r'.*Left_ring.*'),
+        'pinkie': re.compile(r'.*Left_little.*'),
+    }
+    right_patterns = {
+        'thumb': re.compile(r'.*Right_thumb.*'),
+        'index': re.compile(r'.*Right_index.*'),
+        'middle': re.compile(r'.*Right_middle.*'),
+        'ring': re.compile(r'.*Right_ring.*'),
+        'pinkie': re.compile(r'.*Right_little.*'),
+    }
+
     img_folder = os.path.join(abs_path, "SOCOFing")
+
+    if not os.path.exists(male_dirname):
+        os.mkdir(male_dirname)
+    if not os.path.exists(female_dirname):
+        os.mkdir(female_dirname)
+
+    for hand in ['Left', 'Right']:
+        hand_dirname_male = os.path.join(male_dirname, f"{hand} Hand")
+        hand_dirname_female = os.path.join(female_dirname, f"{hand} Hand")
+        if not os.path.exists(hand_dirname_male):
+            os.mkdir(hand_dirname_male)
+        if not os.path.exists(hand_dirname_female):
+            os.mkdir(hand_dirname_female)
+
+        for finger in (left_patterns if hand == 'Left' else right_patterns).items():
+            finger_dirname_male = os.path.join(hand_dirname_male, finger[0])
+            finger_dirname_female = os.path.join(hand_dirname_female, finger[0])
+            if not os.path.exists(finger_dirname_male):
+                os.mkdir(finger_dirname_male)
+            if not os.path.exists(finger_dirname_female):
+                os.mkdir(finger_dirname_female)
+
     for img in os.listdir(img_folder):
-        if male_pattern.match(img):
-            # TO DO: Split the images in the gender directories into directories for each finger
-            if os.path.isdir(male_dirname):
-                shutil.copy(img, male_dirname)   
+        img_path = os.path.join(img_folder, img)
+        
+        if os.path.isfile(img_path):
+            if male_pattern.match(img):
+                # Determine the correct finger directory for males
+                finger_found = False
+                for finger, pattern in male_pattern.items():
+                    if pattern.match(img):
+                        shutil.copy(img_path, os.path.join(male_dirname, f"{hand} Hand", finger))
+                        finger_found = True
+                        break
+                if not finger_found:
+                    shutil.copy(img_path, male_dirname)  # If no specific finger match, place in general male folder
+            elif female_pattern.match(img):
+                # Determine the correct finger directory for females
+                finger_found = False
+                for finger, pattern in female_pattern.items():
+                    if pattern.match(img):
+                        shutil.copy(img_path, os.path.join(female_dirname, f"{hand} Hand", finger))
+                        finger_found = True
+                        break
+                if not finger_found:
+                    shutil.copy(img_path, female_dirname)  # If no specific finger match, place in general female folder
             else:
-                os.mkdir(male_dirname)
-        elif female_pattern.match(img):
-            if os.path.isdir(female_dirname):
-                shutil.copy(img, female_dirname)   
-            else:
-                os.mkdir(female_dirname)
-        else:
-            re_exceptions += 1
+                re_exceptions += 1
 
 def processImages():
     # TO DO: look more into fingerprint recognition techniques/algorithms/etc, the code below will also need to be tweaked
@@ -42,5 +91,6 @@ def processImages():
         img_array = cv2.imread(img)
         img_array = cv2.resize(img_array, (NEW_SIZE, NEW_SIZE))
 
+createDirectories()
 
 print("Number of images that couldn't be categorized: ", re_exceptions)
