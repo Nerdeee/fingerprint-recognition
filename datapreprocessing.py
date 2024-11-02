f24515c1b5b37151150d398a2d0893257e1fb805
Nerdeee <george.larres@gmail.com>
2024-11-01 23:37:45 -0700

    Did more data preprocessing work

--- datapreprocessing.py
+++ datapreprocessing.py
@@ -1,5 +1,7 @@
 # I guess we do option 1 since I'm already implementing the data preprocessing lol
 # Need to ask if using scikit-image built in filters such as sobel and others are considered built in libraries or if we'll have to implement them ourselves
+# One hot encode the labels into one vector. The first 10 elements will encode the left hand from thumb to pinkie, then right hand from pinkie to to thumb, and
+# then the last two elements will encode male and female. Essentially there should always be 2 elements that are hot inside the array. 
 
 import numpy as np
 import cv2
@@ -94,32 +96,25 @@ def createDirectories():
                 re_exceptions += 1
     print("Number of images that couldn't be categorized: ", re_exceptions)
 
-def processImages(X_train, Y_train, X_test, Y_test):
-    # get images (start with female first just to test)                                                 done
-    # perform transformations on image                                                                  half-done
-    # put image inside of the X_train array or some other intermediate value                            half-done
-    # zip X_train and Y_train or some other intermediate array
-    # perform randomization on the previously mentioned array
-    # put 80% of the images into the train section and 20% into the validation section
+#def processImages(X_train, Y_train, X_test, Y_test):
+def processImages(temp_X_array, temp_Y_array):
 
-    temp_X_array = []
-    temp_Y_array = [] 
     def traverse(gender_folder_name):
-        folder_name = os.path.join(abs_path,  gender_folder_name)
+        folder_name = os.path.join(abs_path, gender_folder_name)
         for hand_folder in os.listdir(folder_name):
             hand_folder_path = os.path.join(folder_name, hand_folder)
             for finger_folder in os.listdir(hand_folder_path):
                 finger_folder_path = os.path.join(hand_folder_path, finger_folder)
                 for img_file in os.listdir(finger_folder_path): 
-                    img = os.path.join(finger_folder_path, img_file)
-                    img = cv2.imread(img)
-                    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                    # print('image size: ', img.shape)
-                    temp_X_array.append([img])
-                    temp_Y_array.append(finger_folder)
-        print(f'\nlength of temp X array for  the {gender_folder_name} folder: ', len(temp_X_array))
-        print(f'length of temp Y array for the {gender_folder_name} folder: ', len(temp_Y_array))
-    
+                    img_path = os.path.join(finger_folder_path, img_file)
+                    img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+                    img = cv2.resize(img, (96, 103))
+                    temp_X_array.append(img)  # Append the image array directly
+                    temp_Y_array.append(finger_folder)  # Label for the image
+                    #print('\n\n\n\n\n', temp_Y_array[0], '\n\n\n\n')
+                    #break
+        print(f'Length of temp X array for {gender_folder_name}: ', len(temp_X_array))
+        print(f'Length of temp Y array for {gender_folder_name}: ', len(temp_Y_array))
 
     def transformImage(img):
         sobel_x = np.array([[-1, 0, 1],
@@ -130,41 +125,65 @@ def processImages(X_train, Y_train, X_test, Y_test):
                             [0, 0, 0],
                             [-1, -2, -1]])
         
-        x = np.zeros_like(img)
-        y = np.zeros_like(img)
-
+        x = np.zeros_like(img, dtype=float)
+        y = np.zeros_like(img, dtype=float)
+        img = img.astype(float)  # Ensure the image is in float for calculations
+        
+        # Apply Sobel filter
         for i in range(1, img.shape[0] - 1):
             for j in range(1, img.shape[1] - 1):
                 x[i, j] = np.sum(sobel_x * img[i-1:i+2, j-1:j+2])
                 y[i, j] = np.sum(sobel_y * img[i-1:i+2, j-1:j+2])
         
         magnitudes = np.sqrt(x ** 2 + y ** 2)
-
-        normalized_magnitudes = magnitudes / magnitudes.max()
+        
+        # Normalize magnitudes
+        if np.max(magnitudes) != 0:
+            normalized_magnitudes = magnitudes / np.max(magnitudes)
+        else:
+            normalized_magnitudes = magnitudes  # Avoid division if the max is zero
+        
         return normalized_magnitudes
 
-
+    # Traverse directories for both genders
     traverse(female_dirname)
     traverse(male_dirname)
 
-    # perform transformations
-
-    for img_array in temp_X_array:
-        transformImage(img_array)
-    
-
-
-def splitData():
+    # Perform transformations on images in temp_X_array
+    for i in range(len(temp_X_array)):
+       temp_X_array[i] = transformImage(temp_X_array[i])  # Replace each image with its transformed version
+       print(f"index {i}: ", np.array(temp_X_array[i]).shape)
     return
 
+def encodeImages(temp_X_array, temp_Y_array):
+    print(type(temp_X_array))
+    print(type(temp_Y_array))
+    zipped_array = []
+    for i in range(len(temp_X_array)):
+        zipped_array.append(zip(temp_X_array[i], temp_Y_array[i])) # array of tuples where each tule is of length 2. Tuple[0] = image matrix, tuple[1] = class
+    print(len(zipped_array))
+    
 def main():
+    
     X_train = np.array([])
     Y_train = np.array([])
     X_test = np.array([])
     Y_test = np.array([])
+    
+    temp_X_array = []
+    temp_Y_array = []
     # createDirectories() # only need to call once
-    processImages(X_train, Y_train, X_test, Y_test)
-    splitData()
+    # processImages(X_train, Y_train, X_test, Y_test)
+    # processImages(zipped_images_labels)
+    processImages(temp_X_array, temp_Y_array)
+    print('Length of temp X array: ', len(temp_X_array))
+    print('Length of temp Y array: ', len(temp_Y_array))
+    encodeImages(temp_X_array, temp_Y_array)
+    """ print("X_train shape:", X_train)
+    print("Y_train shape:", Y_train.shape)
+    print("X_test shape:", X_test.shape)
+    print("Y_test shape:", Y_test.shape)
+    """
 
 if __name__ == "__main__":
     main()
